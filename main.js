f6f4757d739fa0bb1024995eee60853a90c1c572
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-11-01 19:29:11 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ function blocks() {
     block.style.display = "none";
   }
 }
+
+const menuIcone = document.querySelector("#btn"),
+  menu = document.querySelector("#Btn"),
+  header = document.querySelector("#mobil");
+
+menuIcone.addEventListener("click", () => {
+  if (header.style.display === "none") {
+    header.style.display = "block";
+  } else {
+    header.style.display = "none";
+  }
+});
+menu.addEventListener("click", () => {
+  if (header.style.display === "none") {
+    header.style.display = "block";
+  } else {
+    header.style.display = "none";
+  }
+});
